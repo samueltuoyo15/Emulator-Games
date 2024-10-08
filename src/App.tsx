b1295e6cb4d71e43bb4177fe63b01af1a639f1af
Oritseweyinmi Samuel Tuoyo <samueltuoyo9082@gmail.com>
2024-10-08 17:13:35 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,20 @@
+import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom';
+import ViewAllGames from './Pages/ViewAllGames'
+import NotFound from './Pages/NotFound'
+import MainLayout from './Layout/MainLayout'
 
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<MainLayout />} />
+        <Route path="/games" element={<ViewAllGames />} />
+        <Route path="*" element={<NotFound />} />
+      </>
+    )
+  )
+
+  return <RouterProvider router={router} />
+}
+
+export default App
