eb2be8bc7bc91d1469a0d849eb974c6b42055109
samueltuoyo15 <samueltuoyo9082@gmail.com>
2024-12-18 08:09:23 +0100

    fixed some bull sheets and som endpoints

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,33 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import {useState, useEffect} from 'react'
-import '@fontsource/poppins/400.css'
-import '@fontsource/poppins/600.css'
-import ViewAllGames from './Pages/ViewAllGames'
-import NotFound from './Pages/NotFound'
-import Login from './Pages/Login'
-import Game from './Pages/Game'
-import Authentication from './Pages/Authentication'
-import MainLayout from './Layout/MainLayout'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/600.css';
+import ViewAllGames from './Pages/ViewAllGames';
+import NotFound from './Pages/NotFound';
+import Login from './Pages/Login';
+import Game from './Pages/Game';
+import Authentication from './Pages/Authentication';
+import MainLayout from './Layout/MainLayout';
 
-interface User{
+interface User {
   email: string;
   id: string;
 }
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => localStorage.getItem('isAuthenticated') === 'true');
-  const [session, setSession] = useState<User[] | null>(null)
+  const [, setIsAuthenticated] = useState<boolean>(() => localStorage.getItem('isAuthenticated') === 'true');
+  const [session, setSession] = useState<User[] | null>(null);
+
+  useEffect(() => {
+    const Status = localStorage.getItem('isAuthenticated');
+    setIsAuthenticated(Status === 'true');
+  }, []);
+
   useEffect(() => {
     const user = localStorage.getItem("user");
     if (user) {
       try {
-        const parsedUser = JSON.parse(user);
+        const parsedUser: User | User[] = JSON.parse(user);
         setSession(Array.isArray(parsedUser) ? parsedUser : [parsedUser]);
       } catch (error) {
         setSession(null);
@@ -30,18 +36,19 @@ function App() {
       setSession(null);
     }
   }, []);
-    return (
-        <Router>
-          <Routes>
-        <Route path="/" element={<MainLayout session={session}/>} />
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout session={session} />} />
         <Route path="/games" element={<ViewAllGames />} />
         <Route path="/auth" element={<Authentication setIsAuthenticated={setIsAuthenticated} />} />
         <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
         <Route path="/games/:id" element={<Game />} />
         <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-    )
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
