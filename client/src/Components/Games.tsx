0bb3296a4d6a5f0d2bb623405c34d73645fc3c43
samueltuoyo15 <samueltuoyo9082@gmail.com>
2024-12-18 08:44:55 +0100

    final push

--- client/src/Components/Games.tsx
+++ client/src/Components/Games.tsx
@@ -12,10 +12,10 @@ function Games({ Menu, SlicedGames }: { Menu: any, SlicedGames: any }) {
         {SlicedGames.map((game: any) => (
           <article key={game.id} onContextMenu={(e) => e.preventDefault()} className="text-center mt-7 w-full md:w-52 rounded-2xl p-2 bg-indigo-800 shadow-2xl">
             <img src={game.album} className="w-full md:w-40 text-center mx-auto" alt="Game album" />
-            <a download href={game.uri} className={`${Menu ? 'hidden' : 'inline-flex'} text-center mt-3 cursor-pointer btn relative z-1 items-center justify-start overflow-hidden font-medium transition-all bg-transparent rounded hover:bg-white group py-2 px-3 border-2 border-purple-600 text-2xl text-white`}>
+            <Link to={`/games/${game.id}`} className={`${Menu ? 'hidden' : 'inline-flex'} text-center mt-3 cursor-pointer btn relative z-1 items-center justify-start overflow-hidden font-medium transition-all bg-transparent rounded hover:bg-white group py-2 px-3 border-2 border-purple-600 text-2xl text-white`}>
               <span className="w-56 h-48 rounded bg-purple-700 absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0 group-hover:border-0"></span>
               <span className="relative w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-white hover:border-0">Download Now</span>
-            </a>
+            </Link>
           </article>
         ))}
       </div>
