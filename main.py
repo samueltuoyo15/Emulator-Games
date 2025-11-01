161e5a0ec0ce8af3baa3d70fdee0f24aa9833d35
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-11-01 23:02:02 +0100

    inital commit

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI(title="Emulator Games Server")
 
 @app.get("/")
 def root():
-    return { "success": True, "message": "API is running"}
+    return { "success": True, "message": "API is running", "data": []}
